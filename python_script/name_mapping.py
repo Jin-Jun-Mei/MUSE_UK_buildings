05837f006f2d1dba50db09b47cba8a09c65aebc0
Jin <xixiaixixi@hotmail.com>
2024-11-24 00:46:11 +0000

    update

---
+++ python_script/name_mapping.py
@@ -0,0 +1,66 @@
+
+## need manually to assign the fuel type for some technologies.
+fuel_mapping = {
+    'ELC': ['.ELC', 'COMPUTERS', 'AIR-CONDITIONER', 'FREEZERS', 'REFRIGERATORS', 'LIGHTING', 'WET-APPLIANCES', 'CONSUMER-ELECTRONICS', 'ELECTRIC', 'ELECTRICITY', 'REFRIGERATION]', '.KETTLE.', '00.ELC', '01.ELC.'],
+    'NGA': ['.GAS.', '.NATURAL-GAS', '.NGA.'],
+    'OIL': ['.OIL'],
+    'SOLAR': ['.SOL', '.SOLAR', '.SOL.', '.SOLAR.'],
+    'HCO': ['.HCO'],
+    'WOOD': ['.WOD'],
+    'LTH': ['.LTH'],
+    'COAL': ['.COL', 'COAL', 'COA'],
+    'HYDROGEN': ['.HYDROGEN', 'HDG', '.HDG.'],
+    'KEROSENE': ['KEROSENE', '.KEROSENE', '.KER.'],
+    'LOGS': ['.LOGS'],
+    'LFO': ['LIGHT-FUEL-OIL', '.LFO'],
+    'LPG': ['LIQUEFIED-PETROLEUM-GAS', '.LPG'],
+    'NGA-BOM-HYG': ['.NGA-BOM-HYG'],
+    'COKE': ['.COKE'],
+    'BOG': ['.BOG'],
+    'BIOMASS': ['.BIOMASS'],
+    'PELLETS': ['.PELLETS', '.PEL'],
+}
+
+
+# residential
+# fuel_mapping = {
+#     'ELC': ['COMPUTERS', 'AIR-CONDITIONER', 'FREEZERS', 'REFRIGERATORS', 'LIGHTING', 'WET-APPLIANCES', 'CONSUMER-ELECTRONICS', 'ELECTRIC','ELECTRICITY','.KETTLE.','00.ELC','.01.ELC.'],
+#     'NGA': ['.GAS.','.NATURAL-GAS','.NGA.'],
+#     'OIL': ['.OIL'],
+#     'HCO': ['.HCO'],
+#     'WOOD': ['.WOD'],
+#     'LTH': ['.LTH'],
+#     'COAL': ['.COL','COAL'],
+#     'HYDROGEN': ['.HYDROGEN','.HDG.'],
+#     'KEROSENE': ['.KEROSENE','.KER.'],
+#     'LOGS': ['.LOGS'],
+#     'LFO': ['LIGHT-FUEL-OIL','.LFO'],
+#     'LPG': ['LIQUEFIED-PETROLEUM-GAS','.LPG'],
+#     'NGA-BOM-HYG': ['.NGA-BOM-HYG'],
+#     'COKE': ['.COKE'],
+#     'PELLETS': ['.PELLETS','.PEL'],
+#     'SOLAR': ['.SOL.', '.SOLAR.'],
+# }
+
+
+## service
+# fuel_mapping = {
+#     'ELC': ['.ELC','COMPUTERS', 'AIR-CONDITIONER', 'FREEZERS', 'REFRIGERATORS', 'LIGHTING', 'WET-APPLIANCES', 'CONSUMER-ELECTRONICS', 'ELECTRIC','ELECTRICITY','REFRIGERATION]'],
+#     'NGA': ['.GAS.','.NATURAL-GAS','.NGA.'],
+#     'OIL': ['.OIL'],
+#     'SOLAR': ['.SOL', '.SOLAR'],
+#     'HCO': ['.HCO'],
+#     'WOOD': ['.WOD'],
+#     'LTH': ['.LTH'],
+#     'COAL': ['.COL','COAL','COA'],
+#     'HYDROGEN': ['.HYDROGEN','HDG'],
+#     'KEROSENE': ['KEROSENE'],
+#     'LOGS': ['.LOGS'],
+#     'LFO': ['LIGHT-FUEL-OIL','.LFO'],
+#     'LPG': ['LIQUEFIED-PETROLEUM-GAS','.LPG'],
+#     'NGA-BOM-HYG': ['.NGA-BOM-HYG'],
+#     'COKE': ['.COKE'],
+#     'BOG': ['.BOG'], 
+#     'BIOMASS': ['.BIOMASS'],
+
+# }
